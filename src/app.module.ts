65d801cfcb0d4ac884d55b91259f88409c97b1b7
Nui-backend-intern <100454921+Mrpk2109@users.noreply.github.com>
2023-04-04 14:46:37 +0700

    chore:add configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,14 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-
-// @Module({
-//   imports: [],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot({
+    load: [configuration],
+  })],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,5 @@
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
-    database: {
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10) || 5432
-    }
+    db_host:process.env.DATABASE_HOST,
+    db_port: parseInt(process.env.DATABASE_PORT, 10)
   });
